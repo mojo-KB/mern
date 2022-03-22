e1817c956725c4d1d782c450a971ffa218615c52
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-03-21 23:57:33 -0400

    hello

---
+++ boilerplate/webpack.config.client.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+const CURRENT_WORKING_DIR = process.cwd();
+
+const config = {
+    name: "browser",
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: [],
+    output: {
+
+    },
+    module: {
+
+    },
+    plugins: {
+
+    },
+    resolve: {
+
+    },
+}
+
+module.exports = config;
